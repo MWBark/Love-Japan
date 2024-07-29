fd769d7eaa0514e459ee233733cead69f20196e1
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-07-29 15:55:49 +0000

    Add SECRET_KEY to env.py

--- lovejapan/settings.py
+++ lovejapan/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!j5k@j+yyg6j%ndonh8-vqidig*2&y0_rhro67-qv2$3r&%=6$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
