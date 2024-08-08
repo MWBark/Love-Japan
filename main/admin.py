66903bf98f6a7575dea2d77af0a5faea8583b3e1
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-08 15:59:30 +0000

    Add ImagePost to admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, ImagePost
 
 # Register your models here.
 admin.site.register(Profile)
+
+@admin.register(ImagePost)
+class ImagePostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
