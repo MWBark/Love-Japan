82730fdb37fa2ed6557254c8d8772d579f73b9f7
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-08 11:34:55 +0000

    Create ImagePost model in main app

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Approved"))
 
 # Create your models here.
 class Profile(models.Model):
@@ -21,3 +24,25 @@ def create_profile(sender, instance, created, **kwargs):
 
 
 post_save.connect(create_profile, sender=User)
+
+
+class ImagePost(models.Model):
+    """
+    Stores a single image post entry related to :model: `auth.User`.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    uploader = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="image_posts"
+    )
+    image = CloudinaryField('image')
+    message = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
