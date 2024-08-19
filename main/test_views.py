309f0a68183ff4b3627f1eacfa101c3f30af2d90
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-19 07:50:06 +0000

    Add tests for 'imagepost' view

--- main/test_views.py
+++ main/test_views.py
@@ -6,18 +6,26 @@ from .models import ImagePost
 # Create your tests here.
 class TestMainViews(TestCase):
 
-        def setUp(self):
-            self.user = User.objects.create_superuser(
-            username="myUsername",
-            password="myPassword",
-            email="test@test.com"
-            )
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+        username="myUsername",
+        password="myPassword",
+        email="test@test.com"
+        )
 
-            self.imagepost = ImagePost(title="image title", uploader=self.user,
-                         slug="image-title", image="",
-                         message="image content", status=1)
-            self.imagepost.save()
+        self.imagepost = ImagePost(title="image title", uploader=self.user,
+                                    slug="image-title", image="",
+                                    message="message content", status=1)
+        self.imagepost.save()
         
-        def test_render_homepage(self):
-            response = self.client.get(reverse('home'))
-            self.assertEqual(response.status_code, 200)
+    def test_render_homepage(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_render_imagepost(self):
+        response = self.client.get(reverse('imagepost'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"image title by myUsername", response.content)
+        self.assertIn(b"message content", response.content)
+        self.assertIn(b"Created on", response.content)
