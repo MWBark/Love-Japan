898af3bba8d31d4493f150bd512e6aa0456bbfee
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-19 07:37:40 +0000

    Create individual image post web pages, linked to in home page.

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ImagePostList.as_view(), name='home'),
+    path('<slug:slug>', views.imagepost, name='imagepost'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import ImagePost
 
@@ -8,3 +8,21 @@ class ImagePostList(generic.ListView):
     queryset = ImagePost.objects.all()
     template_name = "main/index.html"
     paginate_by = 8
+
+
+def imagepost(request, slug):
+    """
+    Display an individual :model:`main.ImagePost`.
+
+    **Context**
+
+    ``imagepost``
+        An instance of :model:`main.ImagePost`.
+
+    **Template:**
+
+    :template:`main/imagepost.html`
+    """
+    queryset = ImagePost.objects.filter(status=1)
+    imagepost = get_object_or_404(queryset, slug=slug)
+    return render(request, 'main/imagepost.html', {"imagepost":imagepost})
