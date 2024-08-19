21399c4ab16930b79f603b98721a3ded9f026b3e
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-19 19:44:33 +0000

    Add test for profile page

--- main/test_views.py
+++ main/test_views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.test import TestCase
-from .models import ImagePost
+from .models import ImagePost, Profile
 
 # Create your tests here.
 class TestMainViews(TestCase):
@@ -17,15 +18,30 @@ class TestMainViews(TestCase):
                                     slug="image-title", image="",
                                     message="message content", status=1)
         self.imagepost.save()
+
         
+        profile = get_object_or_404(Profile, user=self.user)
+        profile.bio = "something about me"
+        profile.save()
+        
+
     def test_render_homepage(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
     
     def test_render_imagepost(self):
-        response = self.client.get(reverse('imagepost'))
+        response = self.client.get(reverse(
+            'imagepost', args=['image-title']))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"image title by myUsername", response.content)
+        self.assertIn(b"image title", response.content)
+        self.assertIn(b"myUsername", response.content)
         self.assertIn(b"message content", response.content)
         self.assertIn(b"Created on", response.content)
+
+    def test_render_profilepage(self):
+        response = self.client.get(reverse(
+            'profile', args=["1"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"myUsername", response.content)
+        self.assertIn(b"something about me", response.content)
