529edcacddac1b7ef1e81c63d628dcad9be604fe
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-08-31 15:27:43 +0000

    Create ContactInfo model in contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class ContactInfo(models.Model):
+    """
+    Stores contact page information.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
